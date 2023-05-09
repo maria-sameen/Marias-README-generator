0fd9824453fbe1bd0d793c8863b0255347596266
Maria S <mariasameen123@hotmail.com>
2023-05-09 21:55:57 +0100

    adding screenshots

--- Assets/utils/generateMarkdown.js
+++ Assets/utils/generateMarkdown.js
@@ -1,8 +1,34 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## description
+  ${data.description}
+  
+  ## table of Contents
+  ${data.tableOfContents}
+  
+  ## installation
+  ${data.installation}
+  
+  ## usage
+  ${data.usage}
+  
+  ## license
+  ${data.license}
+  
+  ## contributing
+  ${data.contributing}
+  
+  ## tests
+  ${data.tests}
+  
+  ## questions
+  If you have any questions or feedback, feel free to reach out to me via [GitHub](https://github.com/${data.github}) or [email](${data.email}).
+  
+  `;
+  }
+  
+  module.exports = generateMarkdown;
+  
 
-`;
-}
-
-module.exports = generateMarkdown;
